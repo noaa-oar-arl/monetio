5c0e2e3ea3656f532d387e7e1cbd2475f769f052
drnimbusrain <patrickc@aaqest.arl.noaa.gov>
2020-10-28 12:49:27 -0400

    Added RRFS_CMAQ attribute.

--- monetio/models/__init__.py
+++ monetio/models/__init__.py
@@ -1,5 +1,5 @@
-from . import camx, cmaq, fv3chem, hysplit, ncep_grib, prepchem
+from . import camx, cmaq, fv3chem, hysplit, ncep_grib, prepchem, cmaqr
 
-__all__ = ['cmaq', 'hysplit', 'camx', 'fv3chem', 'ncep_grib', 'prepchem']
+__all__ = ['cmaq', 'hysplit', 'camx', 'fv3chem', 'ncep_grib', 'prepchem', 'cmaqr']
 
 __name__ = 'models'
