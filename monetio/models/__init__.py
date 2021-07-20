6b3a97bba8b42ad9190a84c779749189aa7dd6cd
mebruckner <48494069+mebruckner@users.noreply.github.com>
2021-07-20 14:43:59 -0500

    Update __init__.py
    Making fixes as Hera install didn't pull hrrr-smoke addition.

--- monetio/models/__init__.py
+++ monetio/models/__init__.py
@@ -3,3 +3,5 @@ from . import camx, cmaq, fv3chem, hysplit, hytraj, ncep_grib, prepchem, pardump
 __all__ = ["cmaq", "hysplit", "camx", "fv3chem", "ncep_grib", "prepchem", "hytraj", "pardump",'hrrr_smoke']
 
 __name__ = "models"
+
+# MEB added HRRR-smoke call 7/20/2021
