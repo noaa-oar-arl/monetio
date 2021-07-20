2148639ef28ee49bd5b68d41b3f578d1b6374ea4
mebruckner <48494069+mebruckner@users.noreply.github.com>
2021-07-20 14:30:03 -0500

    Update __init__.py

--- monetio/models/__init__.py
+++ monetio/models/__init__.py
@@ -1,5 +1,5 @@
-from . import camx, cmaq, fv3chem, hysplit, hytraj, ncep_grib, prepchem, pardump
+from . import camx, cmaq, fv3chem, hysplit, hytraj, ncep_grib, prepchem, pardump, hrrr_smoke
 
-__all__ = ["cmaq", "hysplit", "camx", "fv3chem", "ncep_grib", "prepchem", "hytraj", "pardump"]
+__all__ = ["cmaq", "hysplit", "camx", "fv3chem", "ncep_grib", "prepchem", "hytraj", "pardump",'hrrr_smoke']
 
 __name__ = "models"
