a90fe7ff1bc4e64641f059680f308aa4bb3d5454
mebruckner <48494069+mebruckner@users.noreply.github.com>
2021-07-29 15:13:12 -0500

    Update __init__.py
    add hrrr-smoke, raqms

--- monetio/models/__init__.py
+++ monetio/models/__init__.py
@@ -1,7 +1,5 @@
-from . import camx, cmaq, fv3chem, hysplit, hytraj, ncep_grib, prepchem, pardump, hrrr_smoke
+from . import camx, cmaq, fv3chem, hysplit, hytraj, ncep_grib, prepchem, pardump, hrrr_smoke, raqms_rt
 
-__all__ = ["cmaq", "hysplit", "camx", "fv3chem", "ncep_grib", "prepchem", "hytraj", "pardump",'hrrr_smoke']
+__all__ = ["cmaq", "hysplit", "camx", "fv3chem", "ncep_grib", "prepchem", "hytraj", "pardump",'hrrr_smoke','raqms_rt']
 
 __name__ = "models"
-
-# MEB added HRRR-smoke call 7/20/2021
