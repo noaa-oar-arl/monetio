5e55d608308a075691fef4fe08edb75ea0899648
zmoon <zmoon92@gmail.com>
2022-02-25 17:32:36 -0700

    geoms: start renaming dims

--- monetio/profile/geoms.py
+++ monetio/profile/geoms.py
@@ -51,4 +51,24 @@ def open_dataset(fp):
         attrs=attrs,
     )
 
+    # Set instrument position as coords
+    for vn in ["LATITUDE.INSTRUMENT", "LONGITUDE.INSTRUMENT", "ALTITUDE.INSTRUMENT"]:
+        da = ds[vn]
+        (dim_name0,) = da.dims
+        dim_name = vn.lower().replace(".", "_")
+        ds = ds.set_coords(vn).rename_dims({dim_name0: dim_name})
+
+    # Rename time and scan dims
+    for ref, new_dim in [("DATETIME", "time"), ("ALTITUDE", "scan")]:
+        n = ds[ref].size
+        time_dims = [
+            dim_name
+            for dim_name, dim_size in ds.dims.items()
+            if dim_name.startswith("fakeDim") and dim_size == n
+        ]
+        ds = ds.rename_dims({dim_name: new_dim for dim_name in time_dims})
+
+    # Set time and altitude (dims of a LiDAR scan) as coords
+    ds = ds.set_coords(["DATETIME", "ALTITUDE"])
+
     return ds
