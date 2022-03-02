59ba98c7392e8a7c399c7e6819bb1fc5b95dd8b8
Rebecca Schwantes <rebecca.schwantes@noaa.gov>
2022-03-01 19:50:31 -0700

    add in reader scripts for wrfchem, rrfs-cmaq, and cmaq for MELODIES MONET

--- monetio/models/__init__.py
+++ monetio/models/__init__.py
@@ -1,5 +1,7 @@
-from . import camx, cmaq, fv3chem, hysplit, hytraj, ncep_grib, pardump, prepchem
+from . import camx, cmaq, fv3chem, hysplit, hytraj, ncep_grib, pardump, prepchem 
+from . import _cmaq_mm, _wrfchem_mm, _rrfs_cmaq_mm
 
-__all__ = ["cmaq", "hysplit", "camx", "fv3chem", "ncep_grib", "prepchem", "hytraj", "pardump"]
+__all__ = ["cmaq", "hysplit", "camx", "fv3chem", "ncep_grib", "prepchem", "hytraj", "pardump",
+          "_cmaq_mm","_wrfchem_mm","_rrfs_cmaq_mm"]
 
 __name__ = "models"
