fc65502496c2969204bb8caa9e785da3cc5a66db
zmoon <zmoon92@gmail.com>
2022-03-15 15:11:46 -0600

    geoms: more tests

--- tests/test_geoms.py
+++ tests/test_geoms.py
@@ -9,6 +9,7 @@ from monetio import geoms
 HERE = Path(__file__).parent
 
 TEST_FP = (HERE / "data/tolnet-hdf4-test-data.hdf").absolute().as_posix()
+TEST_FP_H4TONCCF = (HERE / "data/tolnet-hdf4-test-data_h4tonccf_nc4.nc").absolute().as_posix()
 
 
 def test_open():
@@ -25,6 +26,17 @@ def test_open_no_rename_vars():
     assert ds.dims == {"time": 28, "altitude": 496}
 
 
+def test_open_no_squeeze():
+    ds = geoms.open_dataset(TEST_FP, squeeze=False)
+    assert ds.dims == {
+        "latitude_instrument": 1,
+        "longitude_instrument": 1,
+        "altitude_instrument": 1,
+        "time": 28,
+        "altitude": 496,
+    }
+
+
 def test_mjd2k():
     f0 = 0.0
     t0 = pd.Timestamp("2000-01-01 00:00:00")
@@ -36,3 +48,10 @@ def test_mjd2k():
 
     da.attrs.update(VAR_UNITS="MJD2K")
     assert geoms._dti_from_mjd2000(da) == dti
+
+
+def test_cmp_h4tonccf():
+    ds = geoms.open_dataset(TEST_FP, rename_all=False)
+    ds_h4tonccf = xr.open_dataset(TEST_FP_H4TONCCF)
+    # Note: h4tonccf_nc4 replaces all `.` in var names to `_`
+    assert sorted(ds.sizes.values()) == sorted(ds_h4tonccf.squeeze().sizes.values())
