c90ef6da449d32bb005bda0e1fd258259a99f798
zmoon <zmoon92@gmail.com>
2022-06-27 21:28:51 -0600

    aqs: default to parameter_name for non-voc case
    but warn for those we don't have short names for

--- tests/test_aqs.py
+++ tests/test_aqs.py
@@ -1,4 +1,5 @@
 import pandas as pd
+import pytest
 
 from monetio import aqs
 
@@ -6,5 +7,7 @@ from monetio import aqs
 def test_aqs():
     # For MM data proc example
     dates = pd.date_range(start="2019-08-01", end="2019-08-31", freq="H")
+    # Note: will retrieve full year
     network = "NCORE"  # CSN NCORE CASTNET
-    aqs.add_data(dates, param=["PM10SPEC", "SPEC"], network=network, wide_fmt=False, daily=True)
+    with pytest.warns(UserWarning, match="Short names not available for these variables"):
+        aqs.add_data(dates, param=["PM10SPEC", "SPEC"], network=network, wide_fmt=False, daily=True)
