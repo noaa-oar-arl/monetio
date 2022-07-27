865e60beeea2aad14e628e1bb91034ccbb63698f
dwfncar <fillmore@ucar.edu>
2022-07-27 16:24:16 -0600

    Moved _*__mm modules to top.

--- monetio/sat/__init__.py
+++ monetio/sat/__init__.py
@@ -1,12 +1,12 @@
 from . import (
+    _modis_l2_mm,
+    _omps_limb_mm,
+    _omps_nadir_mm,
     goes,
     modis_ornl,
     nesdis_edr_viirs,
     nesdis_eps_viirs,
     nesdis_frp,
-    _modis_l2_mm,
-    _omps_limb_mm,
-    _omps_nadir_mm,
 )
 
 __all__ = [
