d2c5fbb00904b0d40e50232ad4e49aef5b3fdc14
zmoon <zmoon92@gmail.com>
2023-06-05 12:49:36 -0600

    Use OpenSSL version info tup

--- tests/test_aqs.py
+++ tests/test_aqs.py
@@ -1,20 +1,11 @@
 import ssl
-import warnings
 
 import pandas as pd
 import pytest
 
 from monetio import aqs
 
-try:
-    ssl_version = tuple(int(x) for x in ssl.OPENSSL_VERSION.split()[1].split("."))
-except Exception:
-    warnings.warn(
-        "Could not determine OpenSSL version, assuming recent. "
-        f"ssl.OPENSSL_VERSION: {ssl.OPENSSL_VERSION!r}",
-        category=UserWarning,
-    )
-    ssl_version = (100,)
+ssl_version = ssl.OPENSSL_VERSION_INFO
 
 
 @pytest.mark.xfail(ssl_version > (1,), strict=True, reason="Doesn't work with newer OpenSSL")
