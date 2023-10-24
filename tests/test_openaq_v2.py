dd854e1d71742379b628dc74f7534208c8341828
zmoon <zmoon92@gmail.com>
2023-10-24 11:26:38 -0600

    Test get locations

--- tests/test_openaq_v2.py
+++ tests/test_openaq_v2.py
@@ -8,3 +8,15 @@ def test_get_parameters():
     assert params.name.nunique() < len(params), "dupes for different units etc."
     assert "pm25" in params.name.values
     assert "o3" in params.name.values
+
+
+def test_get_locations():
+    sites = openaq.get_locations(npages=2, limit=100)
+    assert len(sites) <= 200
+    assert sites.siteid.nunique() == len(sites)
+    assert sites.dtypes["firstUpdated"] == "datetime64[ns]"
+    assert sites.dtypes["lastUpdated"] == "datetime64[ns]"
+    assert sites.dtypes["latitude"] == "float64"
+    assert sites.dtypes["longitude"] == "float64"
+    assert sites["latitude"].isnull().sum() == 0
+    assert sites["longitude"].isnull().sum() == 0
