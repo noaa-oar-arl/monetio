57e74492aa9f7c612e28cbbd90915ab11280a121
zmoon <zmoon92@gmail.com>
2024-03-14 16:59:52 -0600

    Fix satellite name check

--- tests/test_viirs_aod.py
+++ tests/test_viirs_aod.py
@@ -6,7 +6,7 @@ from monetio.sat.nesdis_viirs_aod_aws_gridded import open_dataset
 @pytest.mark.parametrize("sat", ["SNPP", "NOAA20"])
 @pytest.mark.parametrize("res", [0.05, 0.1, 0.25])
 def test_open_dataset(sat, res):
-    date = "2020-01-01"  # a date when we have both
+    date = "2020-01-01"  # a date when we have both SNPP and NOAA-20 data available
     s_res = f"{res:.3f}"
 
     ds = open_dataset(date, sat, s_res)
@@ -14,5 +14,5 @@ def test_open_dataset(sat, res):
     assert ds.sizes["time"] == 1
     assert ds.sizes["lat"] == int(180 / res)
     assert ds.sizes["lon"] == int(360 / res)
-    assert ds.attrs["satellite_name"] == "NPP" if sat == "NPP" else "NOAA 20"
+    assert ds.attrs["satellite_name"] == ("NPP" if sat == "SNPP" else "NOAA 20")
     assert ds.attrs["spatial_resolution"].strip().startswith(str(res))
