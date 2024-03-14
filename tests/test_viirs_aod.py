de7f76c19f10b7d5761a9f0c928d5b76b09dff1b
zmoon <zmoon92@gmail.com>
2024-03-14 16:51:40 -0600

    Satellite and resolution cases

--- tests/test_viirs_aod.py
+++ tests/test_viirs_aod.py
@@ -1,10 +1,18 @@
+import pytest
+
 from monetio.sat.nesdis_viirs_aod_aws_gridded import open_dataset
 
 
-def test_open_dataset():
+@pytest.mark.parametrize("sat", ["SNPP", "NOAA20"])
+@pytest.mark.parametrize("res", [0.05, 0.1, 0.25])
+def test_open_dataset(sat, res):
     date = "2020-01-01"  # a date when we have both
-    ds = open_dataset(date, "SNPP")
-    assert tuple(ds.dims) == ("time", "lat", "lon")
+    s_res = f"{res:.3f}"
+
+    ds = open_dataset(date, sat, s_res)
+    assert set(ds.dims) == {"time", "lat", "lon"}
     assert ds.sizes["time"] == 1
-    assert ds.attrs["satellite_name"] == "NPP"
-    assert ds.attrs["spatial_resolution"].strip().startswith("0.1")
+    assert ds.sizes["lat"] == int(180 / res)
+    assert ds.sizes["lon"] == int(360 / res)
+    assert ds.attrs["satellite_name"] == "NPP" if sat == "NPP" else "NOAA 20"
+    assert ds.attrs["spatial_resolution"].strip().startswith(str(res))
