2151d75febdb29f1545c39dff0d79e846172507d
zmoon <zmoon92@gmail.com>
2024-03-21 14:38:41 -0600

    Raise errors

--- monetio/models/icap_mme.py
+++ monetio/models/icap_mme.py
@@ -3,16 +3,25 @@ import pandas as pd
 server = "ftp.star.nesdis.noaa.gov"
 base_dir = "/pub/smcd/VIIRS_Aerosol/npp.viirs.aerosol.data/epsaot550/"
 
+valid_filetypes = ("MMC", "C4", "MME")
+valid_data_vars = (
+    "modeaod550",
+    "dustaod550",
+    "pm",
+    "seasaltaod550",
+    "smokeaod550",
+    "totaldustaod550",
+)
 
-def build_urls(dates, *, filetype="mmc", data_var="dustaod550"):
+
+def build_urls(dates, *, filetype="MMC", data_var="dustaod550"):
     """Construct URLs for downloading NEPS data.
 
     Parameters
     ----------
     dates : pd.DatetimeIndex or iterable of datetime
         Dates to download data for.
-    filetype : {'mmc', 'c4'}, optional
-        mmc or c4
+    filetype : {'MMC', 'C4', 'MME'}, optional
     data_var : {'modeaod550', 'dustaod550', 'pm', 'seasaltaod550', \
         'smokeaod550', 'totaldustaod550'}, optional
 
@@ -28,20 +37,16 @@ def build_urls(dates, *, filetype="mmc", data_var="dustaod550"):
         dates = pd.DatetimeIndex(dates)
     else:
         dates = pd.DatetimeIndex([dates])
-    if filetype == "mmc":
-        ft = "MMC"
-    elif filetype == "c4":
-        ft = "C4"
-    else:
-        ft = "MME"
 
     urls = []
     fnames = []
-    print("Building VIIRS URLs...")  #
+    print("Building ICAP-MME URLs...")
     base_url = "https://usgodae.org/ftp/outgoing/nrl/ICAP-MME/"
 
     for dt in dates:
-        fname = "icap_{}_{}_{}.nc".format(dt.strftime("%Y%m%d%H"), ft, data_var.lower())
+        fname = "icap_{}_{}_{}.nc".format(
+            dt.strftime(r"%Y%m%d%H"), filetype.upper(), data_var.lower()
+        )
         url = base_url + dt.strftime(r"%Y/%Y%m/") + fname
         urls.append(url)
         fnames.append(fname)
@@ -92,15 +97,14 @@ def retrieve(url, fname):
         print("\n File Exists: " + fname)
 
 
-def open_dataset(date, product="mmc", data_var="modeaod550"):
+def open_dataset(date, product="MMC", data_var="modeaod550"):
     """
     Parameters
     ----------
     date : str or datetime-like
         The date for which to open the dataset.
         2022-10-29 to current is available.
-    product : {'mmc', 'c4'}, optional
-        mmc or c4
+    product : {'MMC', 'C4', 'MME'}, optional
     data_var : {'modeaod550', 'dustaod550', 'pm', 'seasaltaod550', \
         'smokeaod550', 'totaldustaod550'}, optional
 
@@ -116,39 +120,19 @@ def open_dataset(date, product="mmc", data_var="modeaod550"):
     else:
         d = date
 
-    try:
-        if product.lower() not in ("mmc", "c4", "mme"):
-            raise ValueError
-    except ValueError:
-        print("Invalid input for 'product': Valid values are 'MMC' 'C4' 'MME'")
+    if product.upper() not in valid_filetypes:
+        raise ValueError(f"Invalid input for 'product': Valid values are {valid_filetypes}.")
 
-    try:
-        if data_var.lower() not in (
-            "modeaod550",
-            "dustaod550",
-            "pm",
-            "seasaltaod550",
-            "smokeaod550",
-            "totaldustaod550",
-        ):
-            raise ValueError
-    except ValueError:
-        print(
-            "Invalid input for 'data_var': Valid values are "
-            "'modeaod550' 'dustaod550' 'pm' 'seasaltaod550' 'smokeaod550' 'totaldustaod550'"
-        )
+    if data_var.lower() not in valid_data_vars:
+        raise ValueError(f"Invalid input for 'data_var': Valid values are {valid_data_vars}.")
 
     urls, fnames = build_urls(d, filetype=product, data_var=data_var)
     url = urls.values[0]
     fname = fnames.values[0]
     print(url)
     print(fname)
-    try:
-        if check_remote_file_exists(url) is False:
-            raise ValueError
-    except ValueError:
-        print("File does not exist on ICAP HTTPS server.", url)
-        return ValueError
+    if check_remote_file_exists(url) is False:
+        raise ValueError(f"File does not exist on ICAP HTTPS server: {url}")
     retrieve(url, fname)
 
     dset = xr.open_dataset(fname)
@@ -156,67 +140,47 @@ def open_dataset(date, product="mmc", data_var="modeaod550"):
     return dset
 
 
-def open_mfdataset(dates, product="mmc", data_var="modeaod550"):
+def open_mfdataset(dates, product="MMC", data_var="modeaod550"):
     """
     Parameters
     ----------
     dates : str or datetime-like
         The dates for which to open the dataset.
         2022-10-29 to current is available.
-    product : {'mmc', 'c4'}, optional
-        mmc or c4
+    product : {'MMC', 'C4', 'MME'}, optional
     data_var : {'modeaod550', 'dustaod550', 'pm', 'seasaltaod550', \
         'smokeaod550', 'totaldustaod550'}, optional
 
     Returns
     -------
     xarray.Dataset
+
+    Raises
+    ------
+    ValueError
+        If input parameters are invalid or a file does not exist on the server.
     """
     import pandas as pd
     import xarray as xr
 
-    try:
-        if isinstance(dates, pd.DatetimeIndex):
-            d = dates
-        else:
-            raise TypeError
-    except TypeError:
-        print("Please provide a pandas.DatetimeIndex")
-        return
+    if isinstance(dates, pd.DatetimeIndex):
+        d = dates
+    else:
+        raise TypeError(f"Please provide a pandas.DatetimeIndex. Got {type(dates)}.")
 
-    try:
-        if product.lower() not in ("mmc", "c4", "mme"):
-            raise ValueError
-    except ValueError:
-        print("Invalid input for 'product': Valid values are 'MMC' 'C4' 'MME'")
+    if product.upper() not in valid_filetypes:
+        raise ValueError(f"Invalid input for 'product': Valid values are {valid_filetypes}.")
 
-    try:
-        if data_var.lower() not in (
-            "modeaod550",
-            "dustaod550",
-            "pm",
-            "seasaltaod550",
-            "smokeaod550",
-            "totaldustaod550",
-        ):
-            raise ValueError
-    except ValueError:
-        print(
-            "Invalid input for 'data_var': Valid values are "
-            "'modeaod550' 'dustaod550' 'pm' 'seasaltaod550' 'smokeaod550' 'totaldustaod550'"
-        )
+    if data_var.lower() not in valid_data_vars:
+        raise ValueError(f"Invalid input for 'data_var': Valid values are {valid_data_vars}.")
 
     urls, fnames = build_urls(d, filetype=product, data_var=data_var)
     url = urls.values[0]
     fname = fnames.values[0]
 
     for url, fname in zip(urls, fnames):
-        try:
-            if check_remote_file_exists(url) is False:
-                raise ValueError
-        except ValueError:
-            print("File does not exist on ICAP HTTPS server.", url)
-            return
+        if check_remote_file_exists(url) is False:
+            raise ValueError(f"File does not exist on ICAP HTTPS server: {url}")
         retrieve(url, fname)
 
     dset = xr.open_mfdataset(fnames, combine="nested", concat_dim="time")
