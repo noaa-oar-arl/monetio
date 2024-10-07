f267f801a96f447cf162ce866b346dc8c8eef00c
David Fillmore <fillmore@ucar.edu>
2024-10-07 13:10:37 -0600

    Updated MODIS reader to allow for a list of filenames, renamed swath dims.

--- monetio/sat/_modis_l2_mm.py
+++ monetio/sat/_modis_l2_mm.py
@@ -2,6 +2,7 @@ import logging
 import sys
 from collections import OrderedDict
 from glob import glob
+from datetime import datetime, timezone
 
 import numpy as np
 import xarray as xr
@@ -20,18 +21,23 @@ def read_dataset(fname, variable_dict):
     """
     from monetio.sat.hdfio import hdf_close, hdf_list, hdf_open, hdf_read
 
+    epoch_1993 = int(datetime(1993, 1, 1, tzinfo=timezone.utc).timestamp())
+
     print("reading " + fname)
 
     ds = xr.Dataset()
 
     f = hdf_open(fname)
     hdf_list(f)
-    latitude = hdf_read(f, "Latitude")  # noqa: F841
-    longitude = hdf_read(f, "Longitude")  # noqa: F841
-    start_time = hdf_read(f, "Scan_Start_Time")  # noqa: F841
+    # Geolocation and Time Parameters
+    latitude = hdf_read(f, "Latitude")
+    longitude = hdf_read(f, "Longitude")
+    start_time = hdf_read(f, "Scan_Start_Time") \
+        + epoch_1993 # convert seconds since 1993 to since 1970
     for varname in variable_dict:
         print(varname)
         values = hdf_read(f, varname)
+        print('min, max: ', values.min(), ' ', values.max())
         if "scale" in variable_dict[varname]:
             values = variable_dict[varname]["scale"] * values
         if "minimum" in variable_dict[varname]:
@@ -45,6 +51,10 @@ def read_dataset(fname, variable_dict):
             ds.attrs["quality_flag"] = varname
             ds.attrs["quality_thresh"] = variable_dict[varname]["quality_flag"]
     hdf_close(f)
+
+    ds = ds.assign_coords({'lon': (['dim_0', 'dim_1'], longitude),
+                           'lat': (['dim_0', 'dim_1'], latitude)})
+    ds = ds.rename_dims({'dim_0': 'Cell_Along_Swath', 'dim_1': 'Cell_Across_Swath'})
 
     return ds
 
@@ -83,7 +93,10 @@ def read_mfdataset(fnames, variable_dict, debug=False):
         logging_level = logging.INFO
     logging.basicConfig(stream=sys.stdout, level=logging_level)
 
-    files = sorted(glob(fnames))
+    if isinstance(fnames, list):
+        files = fnames
+    else:
+        files = sorted(glob(fnames))
 
     granules = OrderedDict()
 
