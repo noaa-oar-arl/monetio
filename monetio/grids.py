e40693ce5ffbcdf2a1daa4fe88a879248f5ff886
zmoon <zmoon92@gmail.com>
2025-01-30 12:19:28 -0600

    Add 1 and 5

--- monetio/grids.py
+++ monetio/grids.py
@@ -238,7 +238,10 @@ def _ioapi_grid_from_dataset(ds, *, earth_radius=6370_000):
     }
 
     proj_id = ds.GDTYP
-    if proj_id == 2:
+    if proj_id == 1:
+        # Lat-Lon
+        p4 = "+proj=longlat"
+    elif proj_id == 2:
         # Lambert
         p4 = (
             "+proj=lcc +lat_1={lat_1} +lat_2={lat_2} "
@@ -251,6 +254,9 @@ def _ioapi_grid_from_dataset(ds, *, earth_radius=6370_000):
     elif proj_id == 4:
         # Polar stereo
         p4 = "+proj=stere +lat_ts={lat_1} +lon_0={lon_0} +lat_0=90.0 +x_0=0 +y_0=0 +a={r} +b={r}"
+    elif proj_id == 5:
+        # UTM
+        p4 = "+proj=utm +zone={lat_1:.0f}"
     elif proj_id == 7:
         # Equatorial Mercator
         p4 = "+proj=merc +lat_ts={lat_1} +lon_0={lon_0} +x_0={x0} +y_0={y0} +a={r} +b={r}"
