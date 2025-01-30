e6925986bf4164a9188a343a45372dd5180cc967
zmoon <zmoon92@gmail.com>
2025-01-30 11:50:25 -0600

    Add 7

--- monetio/grids.py
+++ monetio/grids.py
@@ -251,6 +251,9 @@ def _ioapi_grid_from_dataset(ds, *, earth_radius=6370_000):
     elif proj_id == 4:
         # Polar stereo
         p4 = "+proj=stere +lat_ts={lat_1} +lon_0={lon_0} +lat_0=90.0 +x_0=0 +y_0=0 +a={r} +b={r}"
+    elif proj_id == 7:
+        # Equatorial Mercator
+        p4 = "+proj=merc +lat_ts={lat_1} +lon_0={lon_0} +x_0={x0} +y_0={y0} +a={r} +b={r}"
     else:
         raise NotImplementedError("IOAPI proj not implemented yet: {proj_id}")
 
