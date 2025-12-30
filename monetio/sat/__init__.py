c01add04e47c27a797b0de6aedde752e188a3e29
blychs <plichtig@ucar.edu>
2025-12-30 11:43:19 -0700

    pre-commit format fixes

--- monetio/sat/__init__.py
+++ monetio/sat/__init__.py
@@ -1,16 +1,16 @@
 from . import (
+    goes,
     gridded_eos,
     modis_l2,
+    modis_ornl,
     mopitt_l3,
+    nesdis_edr_viirs,
+    nesdis_eps_viirs,
+    nesdis_frp,
     omps_l3,
     omps_nadir,
     tempo_l2,
     tropomi_l2_no2,
-    goes,
-    modis_ornl,
-    nesdis_edr_viirs,
-    nesdis_eps_viirs,
-    nesdis_frp,
 )
 
 __all__ = [
