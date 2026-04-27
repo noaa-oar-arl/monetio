b3e6fb464efcf39bd87078a7179e31df4b80b13f
blychs <plichtig@ucar.edu>
2026-04-27 11:51:58 -0600

    Fixbug: Missiong module in __init__.py

--- monetio/sat/__init__.py
+++ monetio/sat/__init__.py
@@ -11,6 +11,7 @@ from . import (
     omps_nadir,
     tempo_l2,
     tropomi_l2_no2,
+    tropomi_l2,
 )
 
 __all__ = [
@@ -21,6 +22,7 @@ __all__ = [
     "omps_nadir",
     "tempo_l2",
     "tropomi_l2_no2",
+    "tropomi_l2",
     "goes",
     "modis_ornl",
     "nesdis_edr_viirs",
