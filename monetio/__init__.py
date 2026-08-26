5e6ff817031254f8759d8ea7940c7189eeaf91d6
zmoon <zmoon92@gmail.com>
2026-08-26 09:48:03 -0500

    Bump version to 0.3.3

--- monetio/__init__.py
+++ monetio/__init__.py
@@ -29,7 +29,7 @@ from .obs import (
 from .profile import geoms, gml_ozonesonde, icartt, tolnet
 from .sat import goes
 
-__version__ = "0.3.2"
+__version__ = "0.3.3"
 
 __all__ = [
     "__version__",
